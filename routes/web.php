e1dab3ea9dee79f77f761f43e42738ecd3307ef6
NoprauxX12 <juanselizca@outlook.com>
2026-03-04 11:11:06 -0500

    UPDATE | Tutorial 3 part A confusion

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.
 Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
 Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
 Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");
-
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index"); 
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add"); 
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll"); 
 
